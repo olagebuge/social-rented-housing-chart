06a44048d9e3b342f0113fa12ed9840d524aaead
olagebuge <53190225+olagebuge@users.noreply.github.com>
2024-01-22 18:15:55 +0800

    關聯市長

---
+++ mayor.js
@@ -0,0 +1,95 @@
+//總統
+const president = [
+  { name: "李登輝", start: new Date(1990, 4, 20), end: new Date(2000, 4, 20) },
+  { name: "陳水扁", start: new Date(2000, 4, 20), end: new Date(2008, 4, 20) },
+  { name: "馬英九", start: new Date(2008, 4, 20), end: new Date(2016, 4, 20) },
+  { name: "蔡英文", start: new Date(2016, 4, 20), end: new Date(2024, 4, 20) },
+  { name: "賴清德", start: new Date(2024, 4, 20), end: null },
+];
+
+//臺北市長
+const tapeiMayor = [
+  {
+    name: "陳水扁",
+    start: new Date(1994, 12, 25),
+    end: new Date(1998, 12, 25),
+  },
+  {
+    name: "馬英九",
+    start: new Date(1998, 12, 25),
+    end: new Date(2006, 12, 25),
+  },
+  {
+    name: "郝龍斌",
+    start: new Date(2006, 12, 25),
+    end: new Date(2014, 12, 25),
+  },
+  {
+    name: "柯文哲",
+    start: new Date(2014, 12, 25),
+    end: new Date(2022, 12, 25),
+  },
+  { name: "蔣萬安", start: new Date(2022, 12, 25), end: null },
+];
+
+//新北市長
+const newTaipeiMayor = [
+  { name: "蘇貞昌", start: new Date(1997, 12, 20), end: new Date(2004, 5, 20) },
+  { name: "林錫耀", start: new Date(2004, 5, 20), end: new Date(2005, 12, 20) },
+  {
+    name: "周錫瑋",
+    start: new Date(2005, 12, 20),
+    end: new Date(2010, 12, 25),
+  },
+  {
+    name: "朱立倫",
+    start: new Date(2010, 12, 25),
+    end: new Date(2018, 12, 25),
+  },
+  {
+    name: "侯友宜",
+    start: new Date(2018, 12, 25),
+    end: new Date(2026, 12, 25),
+  },
+];
+
+//桃園市長
+const taoyuanMayor = [
+  { name: "朱立倫", start: new Date(2001, 12, 20), end: new Date(2009, 9, 10) },
+  { name: "黃敏恭", start: new Date(2009, 9, 10), end: new Date(2009, 12, 20) },
+  {
+    name: "吳志揚",
+    start: new Date(2009, 12, 20),
+    end: new Date(2014, 12, 25),
+  },
+  {
+    name: "鄭文燦",
+    start: new Date(2014, 12, 25),
+    end: new Date(2022, 12, 25),
+  },
+  { name: "張善政", start: new Date(2022, 12, 25), end: null },
+];
+
+//台中市長
+const taichungMayor = [
+  {
+    name: "張溫鷹",
+    start: new Date(1997, 12, 20),
+    end: new Date(2001, 12, 20),
+  },
+  {
+    name: "胡志強",
+    start: new Date(2001, 12, 20),
+    end: new Date(2014, 12, 25),
+  },
+  {
+    name: "林佳龍",
+    start: new Date(2014, 12, 20),
+    end: new Date(2018, 12, 25),
+  },
+  {
+    name: "盧秀燕",
+    start: new Date(2018, 12, 25),
+    end: new Date(2026, 12, 25),
+  },
+];
