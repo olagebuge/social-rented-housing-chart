a73a4f73317b6b6b4386e23b6990134da54eb1f0
olagebuge <53190225+olagebuge@users.noreply.github.com>
2024-01-23 01:22:12 +0800

    新增南部縣市

--- mayor.js
+++ mayor.js
@@ -93,3 +93,71 @@ const taichungMayor = [
     end: new Date(2026, 12, 25),
   },
 ];
+
+//臺南市長
+const tainanMayor =[
+  {
+    name: "許添財",
+    start: new Date(2001, 12, 20),
+    end: new Date(2010, 12, 25),
+  },
+  {
+    name: "賴清德",
+    start: new Date(2010, 12, 25),
+    end: new Date(2017, 9, 7),
+  },
+  {
+    name: "李孟諺",
+    start: new Date(2017, 9, 7),
+    end: new Date(2018, 12, 25),
+  },
+  {
+    name: "黃偉哲",
+    start: new Date(2018, 12, 25),
+    end: null,
+  }
+]
+
+//高雄市長
+const kaohsiungMayor =[
+  {
+    name: "謝長廷",
+    start: new Date(1998, 12, 25),
+    end: new Date(2005, 2, 1),
+  },
+  {
+    name: "陳其邁",
+    start: new Date(2005, 2, 1),
+    end: new Date(2005, 9, 26),
+  },
+  {
+    name: "葉菊蘭",
+    start: new Date(2005, 9, 26),
+    end: new Date(2006, 12, 25),
+  },
+  {
+    name: "陳菊",
+    start: new Date(2006, 12, 25),
+    end: new Date(2018, 4, 22),
+  },
+  {
+    name: "許立明",
+    start: new Date(2018, 4, 23),
+    end: new Date(2018, 12, 25),
+  },
+  {
+    name: "韓國瑜",
+    start: new Date(2018, 12, 25),
+    end: new Date(2020, 6, 12),
+  },
+  {
+    name: "楊明州",
+    start: new Date(2020, 6, 13),
+    end: new Date(2020, 8, 24),
+  },
+  {
+    name: "陳其邁",
+    start: new Date(2020, 8, 24),
+    end: null,
+  }
+]
